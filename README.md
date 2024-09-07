ddbd24557bc5cdd993ac6dd082273eb44b725cee
qianyu <ming>
2024-09-07 15:58:38 +0800

    fist_commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# ai-paper
+
+English | [简体中文](./README-zh.md)
+
+> A minimal vue admin template with Element UI & axios & iconfont & permission control & lint
+
+
+```bash
+# clone the project
+git clone https://github.com/ai-paper.git
+
+# enter the project directory
+cd ai-paper
+
+# install dependency
+npm install
+
+# develop
+npm run dev
+```
+
+This will automatically open http://localhost:9528
+
+## Build
+
+```bash
+# build for test environment
+npm run build:stage
+
+# build for production environment
+npm run build:prod
+```
+
+## Advanced
+
+```bash
+# preview the release environment effect
+npm run preview
+
+# preview the release environment effect + static resource analysis
+npm run preview -- --report
+
+# code format check
+npm run lint
+
+# code format check and auto fix
+npm run lint -- --fix
+```
+
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <el-row :gutter="10">
+      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"
+        ><div class="grid-content bg-purple-light"></div
+      ></el-col>
+      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"
+        ><div class="grid-content bg-purple"></div
+      ></el-col>
+      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"
+        ><div class="grid-content bg-purple-light"></div
+      ></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "home",
+  computed: {
+    ...mapGetters(["name"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
