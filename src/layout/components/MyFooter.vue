302efa2b2e63685d32b44a47703f68c1a748f24d
dfine <coding@dfine.tech>
2024-09-11 23:03:37 +0800

    fix: rename myFooter to MyFooter

---
+++ src/layout/components/MyFooter.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="footer">
+        <!-- 底部栏 -->
+        <p style="text-align: center;color:#666;">© 2024 北京智思未来科技有限公司海南分公司 关于我们 京ICP备2024042149号-5 京公网安备11010802043865</p>
+    </div>
+</template>
+<script>
+// import { mapGetters } from "vuex";
+// import { sms } from "@/api/login";
+// import webinfo from "@/components/webinfo.vue";
+
+export default {
+    name: "myFooter",
+    data() {
+        return {
+            // 定义变量
+        };
+    },
+    components: {
+        // webinfo,
+    },
+    mounted() {
+        // 页面初始化
+    },
+
+    computed: {
+        // 计算属性
+    },
+    methods: {
+        // 定义方法
+    },
+};
+</script>
+<style lang="scss" scoped>
+// 引入scss
+// @import './index.scss';
+
+// 媒体查询
+// @media only screen and (max-width: 939px) {
+// }
+.footer {
+    padding: 15px 0;
+}
+.footer p {
+    margin: 0px;
+}
+</style>
