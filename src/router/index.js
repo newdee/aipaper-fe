a59698b32ce617c344158624313e73c5ed78942f
qianyu <ming>
2024-09-11 21:29:25 +0800

    测试推送双端

--- src/router/index.js
+++ src/router/index.js
@@ -123,19 +123,7 @@ export const constantRoutes = [{
     ]
   },
 
-  {
-    path: '/form',
-    component: Layout,
-    children: [{
-      path: 'index',
-      name: 'Form',
-      component: () => import('@/views/form/index'),
-      meta: {
-        title: 'Form',
-        icon: 'form'
-      }
-    }]
-  },
+  
 
   {
     path: '/nested',
