c1e5713a8e5d90b134608a844f1a2b49807e67a6
Qiao-0527 <975155628@qq.com>
2024-09-23 23:27:32 +0800

    细节

--- src/layout/components/UserOrders.vue
+++ src/layout/components/UserOrders.vue
@@ -6,17 +6,29 @@
     </div>
     <!-- 订单列表 -->
     <el-checkbox-group v-model="checkList" @change="handleCheckAllChange">
-      <div class="orderBox" v-for="orderObj in orderList" :key="orderObj.order.id">
-        <el-checkbox :label="orderObj.order.id" :value="orderObj.order.id"></el-checkbox>
+      <div
+        class="orderBox"
+        v-for="orderObj in orderList"
+        :key="orderObj.order.id"
+      >
+        <el-checkbox
+          :label="orderObj.order.id"
+          :value="orderObj.order.id"
+        ></el-checkbox>
         <div class="order">
           <div class="orderNum rowBetween">
             <!-- <div class="left">订单号：{{ orderObj.order.out_trade_no }}</div> -->
             <div class="left"></div>
             <div class="right">时间：{{ orderObj.order.created_at }}</div>
           </div>
-          <template v-for="(item,j) in orderObj.order_item_response">
-            <div class="orderTitle" :key="item.paper.title +j">{{ item.paper.title }}</div>
-            <div class="orderText rowBetween handleRow" :key="item.product.id +j">
+          <template v-for="(item, j) in orderObj.order_item_response">
+            <div class="orderTitle" :key="item.paper.title + j">
+              {{ item.paper.title }}
+            </div>
+            <div
+              class="orderText rowBetween handleRow"
+              :key="item.product.id + j"
+            >
               <div class="left">{{ item.product.name }}</div>
               <div class="right">
                 <span class="handle">下载</span>
@@ -48,8 +60,12 @@
             <div class="left">订单价格</div>
             <div class="right">
               <span class="price">￥{{ orderObj.order.total_price }}</span>
-              <span class="handle" v-if="orderObj.order.payment_status == 'WAIT_BUYER_PAY'" style="color: crimson;">去支付</span>
-              
+              <span
+                class="handle"
+                v-if="orderObj.order.payment_status == 'WAIT_BUYER_PAY'"
+                style="color: crimson"
+                >去支付</span
+              >
             </div>
           </div>
         </div>
@@ -145,6 +161,28 @@ export default {
   max-height: 90vh;
   overflow: auto;
 }
+/* 整个滚动条 */
+.ordersList::-webkit-scrollbar {
+  width: 8px; /* 滚动条宽度 */
+  background-color: #f5f5f5; /* 背景颜色 */
+}
+
+/* 滚动条轨道 */
+.ordersList::-webkit-scrollbar-track {
+  border-radius: 8px;
+  background-color: #f5f5f5;
+}
+
+/* 滚动条滑块 */
+.ordersList::-webkit-scrollbar-thumb {
+  border-radius: 8px;
+  background-color: #555;
+}
+
+/* 当鼠标悬停在滑块上时的样式 */
+.ordersList::-webkit-scrollbar-thumb:hover {
+  background-color: #666;
+}
 .orderBox {
   display: flex;
   flex-wrap: nowrap;
@@ -196,6 +234,6 @@ export default {
 }
 .handle {
   cursor: pointer;
-  margin-left:5px;
+  margin-left: 5px;
 }
 </style>
