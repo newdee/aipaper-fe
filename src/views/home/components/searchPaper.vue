797e07445f1652b784b66c12dd82d0b38a5ad414
qianyu <ming>
2024-09-27 00:19:57 +0800

    分割组件测试

---
+++ src/views/home/components/searchPaper.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <!-- 页面名称 -->
+    <div class="mainBackground">
+      <div>
+        <p>MIXPAPER</p>
+        <p>万象学术</p>
+      </div>
+      <div>
+        <p>AI 论文快插</p>
+        <div class="selectText">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </div>
+        <!-- 输入框 -->
+        <div class="inputBox">
+          <input type="text" />
+        </div>
+        <div>
+          <span>
+            <i class="el-icon-search"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+// import { mapGetters } from "vuex";
+// import { sms } from "@/api/login";
+// import webinfo from "@/components/webinfo.vue";
+// import eventBus from "@/utils/eventBus";
+
+export default {
+  name: "myFooter",
+  data() {
+    return {
+      // 定义变量
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+      ],
+      value: "",
+    };
+  },
+  components: {
+    // webinfo,
+  },
+  mounted() {
+    // eventBus.emit("sendOutline", 5); // 发布事件
+    // 页面初始化
+  },
+  created() {
+    // eventBus.on("sendOutline", this.addE); // 订阅事件
+  },
+  beforeDestroy() {
+    // eventBus.off("sendOutline", this.addE); // 移除事件监听
+  },
+  computed: {
+    // 计算属性
+  },
+  methods: {
+    // 定义方法
+  },
+};
+</script>
+<style lang="scss" scoped>
+// 引入scss
+// @import "@/styles/variables.scss";
+// @import './index.scss';
+
+// 媒体查询
+// @media only screen and (max-width: 939px) {
+// }
+.mainBackground {
+  background: url("../../../assets/images/newspaper.png") no-repeat 0 0;
+  background-size: cover;
+  height: 612px;
+  width: 100%;
+}
+</style>
