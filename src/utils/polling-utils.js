795bafed7f7378d677b5ad30fc433f1b82e1bfa8
qianyu <ming>
2024-10-14 23:32:44 +0800

    pdf预览及支付轮询

--- src/utils/polling-utils.js
+++ src/utils/polling-utils.js
@@ -1,25 +1,43 @@
-/**
- * @descripting 轮询功能
- * @param {String} type 请求类型
- * @param {String} url 地址
- * @param {Object} data 请求数据
- * @param {Number} delay 轮询间隔时间
- */
-
 import { outlineStatus } from "@/api/user";
 
-export default function polling(data, delay = 2000) {
+/**
+ * 实现轮询功能
+ * @param {Object} data 请求数据
+ * @param {Number} delay 轮询间隔时间，默认为2000毫秒
+ * @param {Number} maxRetries 最大重试次数，默认为5次
+ * @param {Number} currentRetry 当前重试次数，默认为0
+ */
+export default function polling(
+  data,
+  delay = 2000,
+  maxRetries = 5,
+  currentRetry = 0
+) {
   return new Promise((resolve, reject) => {
-    outlineStatus(data).then((res) => {
-      console.log("res", res.result);
-      if (res.result.status == "生成中") {
-        // 这个继续进行轮询的条件，需要根据自己的需要修改
-        setTimeout(() => {
-          resolve(polling(data, delay));
-        }, delay);
-      } else {
-        resolve(res);
-      }
-    });
+    outlineStatus(data)
+      .then((res) => {
+        console.log("res", res.result);
+        if (res.result.status === "生成中") {
+          // 如果状态是生成中，则等待一段时间后再次调用自身
+          setTimeout(() => {
+            resolve(polling(data, delay, maxRetries, currentRetry));
+          }, delay);
+        } else {
+          // 如果不是生成中状态，则解析结果
+          resolve(res);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        if (currentRetry + 1 < maxRetries) {
+          // 如果当前重试次数小于最大重试次数，则等待一段时间后再次尝试
+          setTimeout(() => {
+            resolve(polling(data, delay, maxRetries, currentRetry + 1));
+          }, delay);
+        } else {
+          // 如果已经达到最大重试次数，则拒绝Promise
+          reject(new Error("Max retries reached"));
+        }
+      });
   });
 }
