3687e2772b9611f03244b31098d3b6b58f848868
qianyu <ming>
2024-10-17 23:03:38 +0800

    fix:查看大纲bug修改

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   const { mockXHR } = require("../mock");
   mockXHR();
 }
-import locale from 'element-ui/lib/locale/lang/zh-CN';
+import locale from "element-ui/lib/locale/lang/zh-CN";
 // set ElementUI lang to EN
 Vue.use(ElementUI, {
   locale,
@@ -57,15 +57,8 @@ Vue.prototype.$minWidth = 500;
 import VueScrollTo from "vue-scrollto";
 
 let scrollOptions = {
-  container: "body", //滚动的容器
-  duration: 500, //滚动时间
   easing: "ease", //缓动类型
   offset: -50, // 滚动时应应用的偏移量。此选项接受回调函数
-  force: true, // 是否应执行滚动
-  cancelable: true, // 用户是否可以取消滚动
-  onStart: false, // 滚动开始时的钩子函数
-  onDone: false, // 滚动结束时候的钩子函数
-  onCancel: false, // 用户取消滚动的钩子函数
   x: false, // 是否要在x轴上也滚动
   y: true, // 是否要在y轴上滚动
 };
