ddfb79014020104570ebbfb4040a968a17606e79
qianyu <ming>
2024-10-24 22:06:37 +0800

    图片替换

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,14 +1,11 @@
 // translate router.meta.title, be used in breadcrumb sidebar tagsview
 export function generateTitle(title) {
   const hasKey = this.$te("route." + title);
-  console.log("dddd", title);
-  console.log("hasKey", hasKey);
   const translatedTitle = "";
 
   if (hasKey) {
     // $t :this method from vue-i18n, inject in @/lang/index.js
     const translatedTitle = this.$t("route." + title);
-    console.log("translatedTitle", translatedTitle);
 
     return translatedTitle;
   }
