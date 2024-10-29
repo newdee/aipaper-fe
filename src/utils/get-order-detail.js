e9e923d83bac1136b5e25f4ede88d290be386a44
qianyu <ming>
2024-10-29 23:42:20 +0800

    feat: slider

--- src/utils/get-order-detail.js
+++ src/utils/get-order-detail.js
@@ -20,29 +20,34 @@ export default function polling(
         let givenTimeString = res.result.aitype_time;
         // 将给定的时间字符串转换为时间戳
         const givenTimestamp = Date.parse(givenTimeString.replace(/-/g, "/")); // 替换日期中的 "-" 为 "/" 以兼容所有浏览器
+        const givenTimeEnd =
+          Date.parse(givenTimeString.replace(/-/g, "/")) + 180000; // 替换日期中的 "-" 为 "/" 以兼容所有浏览器
 
         // 获取当前时间的时间戳
         const currentTimestamp = Date.now();
-
-        // 比较两个时间戳，判断是否超时
-        if (res.result.status === "生成中") {
-          if (givenTimestamp > currentTimestamp) {
-            console.log("给定的时间还未到达，未超时。");
-
-            // 如果状态是生成中，则等待一段时间后再次调用自身
-            setTimeout(() => {
-              resolve(polling(data, delay, maxRetries, currentRetry));
-            }, delay);
-          } else {
-            reject(new Error("已超时"));
-            console.log("给定的时间已经过去，超时。");
-            return false;
-          }
-        } else if (res.result.status == "生成失败") {
-          resolve(res.result.status);
+        // 判断循环开始时间，如果任务没开始 继续循环
+        if (currentTimestamp < givenTimestamp) {
+          resolve(polling(data, delay, maxRetries, currentRetry));
         } else {
-          // 如果不是生成中状态，则解析结果
-          resolve(res.result.outline.outline);
+          if (res.result.status === "生成中") {
+            // 比较两个时间戳，判断是否超时
+            if (givenTimeEnd > currentTimestamp) {
+              console.log("给定的时间还未到达，未超时。");
+              // 如果状态是生成中，则等待一段时间后再次调用自身
+              setTimeout(() => {
+                resolve(polling(data, delay, maxRetries, currentRetry));
+              }, delay);
+            } else {
+              reject(new Error("已超时"));
+              console.log("给定的时间已经过去，超时。");
+              return false;
+            }
+          } else if (res.result.status == "生成失败") {
+            resolve(res.result.status);
+          } else {
+            // 如果不是生成中状态，则解析结果
+            resolve(res.result.outline.outline);
+          }
         }
       })
       .catch((error) => {
