58b91d74365af333540a99413eec7a267bd7b96c
Qiao-0527 <975155628@qq.com>
2024-11-07 23:23:09 +0800

    最近生成的论文

--- src/views/writepaper/components/step0.vue
+++ src/views/writepaper/components/step0.vue
@@ -1,49 +1,121 @@
 <template>
   <div>
-    <!-- 页面名称 -->
-    <div class="outlineListClass">
-      <div class="lineNav">
-        <p>最近创建</p>
-      </div>
-      <div v-for="(orderObj, index) in orderList" :key="index + 'outlineList'">
-        <div class="order">
-          <div>
-            <div class="orderTitle">{{ orderObj.title }}</div>
-            <div class="orderText rowBetween handleRow">
-              <div class="left">大纲状态：{{ orderObj.status }}</div>
-              <div class="right" @click="jumpStep2(orderObj)">
-                <el-button
-                  icon="el-icon-view"
-                  :disabled="orderObj.status != '生成成功'"
-                  type="text"
-                  >查看大纲</el-button
-                >
+    <!-- 最近创建的大纲 最近生成的论文 -->
+    <div :class="[device == 'desktop' ? 'recentListBox' : '']">
+      <div class="outlineListClass">
+        <div class="lineNav">
+          <p>最近创建的大纲</p>
+        </div>
+        <div
+          v-for="(orderObj, index) in outlinesList"
+          :key="index + 'outlineList'"
+        >
+          <div class="order">
+            <div>
+              <div class="orderTitle">{{ orderObj.title }}</div>
+              <div class="orderText rowBetween handleRow">
+                <div class="left">大纲状态：{{ orderObj.status }}</div>
+                <div class="right" @click="jumpStep2(orderObj)">
+                  <el-button
+                    icon="el-icon-view"
+                    :disabled="orderObj.status != '生成成功'"
+                    type="text"
+                    >查看大纲</el-button
+                  >
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="orderText rowStart handleRow">
-            <div class="left">
-              类型:
-              <span class="price">{{ orderObj.type }}</span>
-            </div>
-            <div class="left">
-              科目:
-              <span class="price">{{ orderObj.field }}</span>
-            </div>
-            <div class="left">
-              生成时间：
-              <span class="price">{{
-                orderObj.updated_at | dateFormatter
-              }}</span>
-            </div>
-            <!-- <span class="price">￥: 0.01元</span> -->
-            <!-- <span
+            <div class="orderText rowStart handleRow">
+              <div class="left">
+                类型:
+                <span class="price">{{ orderObj.type }}</span>
+              </div>
+              <div class="left">
+                科目:
+                <span class="price">{{ orderObj.field }}</span>
+              </div>
+              <div class="left">
+                生成时间：
+                <span class="price">{{
+                  orderObj.updated_at | dateFormatter
+                }}</span>
+              </div>
+              <!-- <span class="price">￥: 0.01元</span> -->
+              <!-- <span
                 class="handle"
                 v-if="orderObj.order.payment_status == 'WAIT_BUYER_PAY'"
                 style="color: crimson"
                 >去支付</span
               > -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="outlineListClass">
+        <div class="lineNav">
+          <p>最近生成的论文</p>
+        </div>
+        <div
+          class="order"
+          v-for="(orderObj, index) in orderList"
+          :key="index + 'outlineList'"
+        >
+          <template>
+            <div class="orderTitle" :key="'case2' + index">
+              {{ orderObj.outline.title }}
+            </div>
+            <div
+              v-if="orderObj.order_item_response.length > 0"
+              class="orderText rowBetween handleRow"
+              :key="'case3' + index"
+            >
+              <div class="left">
+                生成状态：
+                {{
+                  orderObj.order_item_response[0].case.paper_case.stage
+                    | orderStatusFormatter
+                }}
+              </div>
+              <div class="right">
+                <el-button
+                  v-if="orderObj.order_item_response[0].case.paper_case.stage"
+                  icon="el-icon-download"
+                  :disabled="
+                    orderObj.order_item_response[0].case.paper_case.stage !=
+                      2 || downStatus
+                  "
+                  type="text"
+                  @click="downLoadPaper(orderObj)"
+                >
+                  下载
+                </el-button>
+              </div>
+            </div>
+            <div
+              v-else
+              class="orderText rowBetween handleRow"
+              :key="'case3' + index"
+            >
+              <div class="left">生成状态：未生成</div>
+              <div class="right"></div>
+            </div>
+          </template>
+          <div class="orderText rowStart handleRow">
+            <div class="left">
+              类型:
+              <span class="price">{{ orderObj.outline.type }}</span>
+            </div>
+            <div class="left">
+              科目:
+              <span class="price">{{ orderObj.outline.field }}</span>
+            </div>
+            <div class="left">
+              生成时间：
+              <span class="price">{{
+                orderObj.order.created_at | dateFormatter
+              }}</span>
+            </div>
           </div>
         </div>
       </div>
@@ -51,11 +123,11 @@
   </div>
 </template>
 <script>
-// import { mapGetters } from "vuex";
+import { mapGetters } from "vuex";
 // import { sms } from "@/api/login";
 // import webinfo from "@/components/webinfo.vue";
 import eventBus from "@/utils/eventBus";
-import { getOutlineList } from "@/api/user";
+import { getOutlineList, getOrderList, paperPack } from "@/api/user";
 import { throttle } from "lodash";
 import { getToken } from "@/utils/auth"; //
 
@@ -64,7 +136,9 @@ export default {
   data() {
     return {
       // 定义变量
-      orderList: [],
+      outlinesList: [], // 大纲列表
+      orderList: [], // 订单列表
+      downStatus: false,
     };
   },
   components: {
@@ -88,6 +162,7 @@ export default {
   },
   computed: {
     // 计算属性
+    ...mapGetters(["device"]),
   },
   methods: {
     jumpStep2(row) {
@@ -115,22 +190,57 @@ export default {
         });
     },
     // 定义方法
-    handleCurrentChange: throttle(function () {
+    handleCurrentChange: throttle(async function () {
       // 这里可以添加你的分页逻辑，例如发送请求获取新的数据
       let params = {
         page_num: 1,
         page_size: 5,
       };
+      // 查询大纲
       getOutlineList(params).then((res) => {
         let data = res.result;
         if (Object.keys(data).length > 0) {
-          this.orderList = data.outline_list || [];
-          console.log("order", this.orderList);
+          this.outlinesList = data.outline_list || [];
+          console.log("180---outlines", this.outlinesList);
           // this.page.page_num = data.page_num - 0;
           // this.page.total = data.total;
         }
       });
+      // 查询订单
+      getOrderList(params).then((res) => {
+        let data = res.result;
+        if (Object.keys(data).length > 0) {
+          this.orderList = data.order_resp_list || [];
+          console.log("196---orderList", this.orderList);
+        }
+      });
     }, 300), // 300毫秒内最多执行一次
+
+    downLoadPaper: _.debounce(function (item) {
+      console.log("item", item);
+      this.downStatus = true;
+      paperPack({ out_trade_no: item.order.out_trade_no }).then((res) => {
+        console.log("ad", res.result.zip_url);
+        this.downStatus = false;
+        // window.open(res.result.zip_url, "_blank");
+        // Create a temporary link element
+        const link = document.createElement("a");
+        link.href = res.result.zip_url;
+
+        // Set the download attribute to suggest a filename
+        link.download =
+          item.order_item_response[0].case.paper_case.title + ".zip"; // Change 'filename.zip' to the desired file name
+
+        // Append the link to the body
+        document.body.appendChild(link);
+
+        // Programmatically click the link to trigger the download
+        link.click();
+
+        // Remove the link from the document
+        document.body.removeChild(link);
+      });
+    }, 300),
   },
 };
 </script>
@@ -139,7 +249,16 @@ export default {
 // @import "@/styles/variables.scss";
 // @import "@/styles/mediaMain.scss";
 // @import './index.scss';
+.recentListBox {
+  display: grid;
+  grid-template-columns: 1fr 1fr; /* 两列，每列占用相等的空间 */
+  grid-gap: 10px; /* 格子间隔 */
+  .outlineListClass {
+    margin-bottom: 0px;
+  }
+}
 .outlineListClass {
+  margin-bottom: 10px;
   .ordersList {
     --border: 1px solid #0000001f;
     --border-bottom: 1px solid #0000001f;
@@ -209,12 +328,11 @@ export default {
   }
   .rowStart {
     display: flex;
-    justify-content: flex-start;
     line-height: 24px;
+    justify-content: space-between;
   }
 
   .left {
-    width: 300px;
     color: #666;
   }
 
@@ -231,6 +349,8 @@ export default {
     color: #202020;
     font-weight: 600;
     font-size: 15px;
+    line-height: 1.5em;
+    min-height: 1.5em;
   }
 
   .handleRow:not(:last-child) {
