6e3873bde27c99567cbecf300136a5ee248c4513
qianyu <ming>
2024-12-05 00:33:18 +0800

    chat

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,90 +1,169 @@
 <template>
-  <div class="chat-container">
-    <!-- <div class="navChat">
-      <el-button>清屏</el-button>
-    </div> -->
-    <div v-show="chatMessages.length <= 0" class="defaultText">
-      <p>
-        <span><img src="@/assets/images/logoMax.png" alt="" /></span>
-        万象AI, 您的私人助手!
-      </p>
-    </div>
-    <div class="chat-messages">
-      <div
-        :class="['messageBox', msg.type]"
-        v-for="(msg, index) in chatMessages"
-        :key="index"
-      >
-        <div class="userInfo">
-          <img v-if="msg.type === 'user'" :src="avatar" alt="" />
-          <img
-            v-if="msg.type === 'response'"
-            :src="logoMax"
-            alt=""
-            class="logoMax"
-          />
-        </div>
-        <div :class="['message', msg.type]">
-          <div
-            :class="[
-              'infoList',
-              msg.type == 'user' ? 'userTitle' : 'answerTitle',
-            ]"
-          >
-            <div v-html="renderMarkdown(msg.text)"></div>
-            <div v-if="msg.type === 'response'" class="responseBottom">
-              <div></div>
-              <el-tooltip
-                class="item"
-                effect="dark"
-                content="重新生成"
-                placement="top"
-              >
-                <el-button
-                  size="mini"
-                  circle
-                  @click="regenerateResponse(index - 1)"
-                  class="reload-icon"
-                >
-                  <svg class="icon svg-icon" aria-hidden="true">
-                    <use xlink:href="#icon-reload"></use>
-                  </svg>
-                </el-button>
-              </el-tooltip>
-            </div>
-          </div>
-        </div>
-        <button
-          v-if="msg.type === 'user'"
-          @click="openEditDialog(index)"
-          class="edit-icon"
-        >
-          <svg class="icon svg-icon" aria-hidden="true">
-            <use xlink:href="#icon-edit"></use>
-          </svg>
-        </button>
+  <div class="chatMain">
+    <div class="sidebarLeft">
+      <div class="sliderContent">
+        <span>asdfasdf</span>
       </div>
     </div>
-    <div class="chat-input">
-      <el-input
-        type="textarea"
-        placeholder="请输入您的问题"
-        v-model="message"
-        @keydown.native="handleKeydown"
-      >
-      </el-input>
-      <button @click="sendMessage">
-        <i class="el-icon-search"></i>
-      </button>
+    <div class="chat-container">
+      <!-- <div class="navChat">
+      <el-button>清屏</el-button>
+    </div> -->
+      <div v-show="chatMessages.length <= 0" class="defaultText">
+        <p>
+          <span><img src="@/assets/images/logoMax.png" alt="" /></span>
+          万象AI, 您的私人助手!
+        </p>
+      </div>
+      <div class="chat-messages">
+        <div
+          :class="['messageBox', msg.type]"
+          v-for="(msg, index) in chatMessages"
+          :key="index"
+        >
+          <div class="userInfo">
+            <img v-if="msg.type === 'user'" :src="avatar" alt="" />
+            <img
+              v-if="msg.type === 'response'"
+              :src="logoMax"
+              alt=""
+              class="logoMax"
+            />
+          </div>
+          <div :class="['message', msg.type]">
+            <div
+              :class="[
+                'infoList',
+                msg.type == 'user' ? 'userTitle' : 'answerTitle',
+              ]"
+            >
+              <div v-html="renderMarkdown(msg.text)"></div>
+              <div v-if="msg.type === 'response'" class="responseBottom">
+                <div></div>
+                <el-tooltip
+                  class="item"
+                  effect="dark"
+                  content="重新生成"
+                  placement="top"
+                >
+                  <el-button
+                    size="mini"
+                    circle
+                    @click="regenerateResponse(index - 1)"
+                    class="reload-icon"
+                  >
+                    <svg class="icon svg-icon" aria-hidden="true">
+                      <use xlink:href="#icon-reload"></use>
+                    </svg>
+                  </el-button>
+                </el-tooltip>
+              </div>
+            </div>
+          </div>
+          <button
+            v-if="msg.type === 'user'"
+            @click="openEditDialog(index)"
+            class="edit-icon"
+          >
+            <svg class="icon svg-icon" aria-hidden="true">
+              <use xlink:href="#icon-edit"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div class="chatBottom GH">
+        <div class="chatBottomHeader">
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="切换模型"
+            placement="top"
+          >
+            <div class="workItem">
+              <svg class="icon svg-icon" aria-hidden="true">
+                <use xlink:href="#icon-OpenAI"></use>
+              </svg>
+            </div>
+          </el-tooltip>
+
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="上传图片"
+            placement="top"
+          >
+            <div class="workItem">
+              <i class="el-icon-picture-outline-round"></i>
+            </div>
+          </el-tooltip>
+
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="上传文件"
+            placement="top"
+          >
+            <div class="workItem">
+              <i class="el-icon-paperclip"></i>
+            </div>
+          </el-tooltip>
+          <div class="workItem">
+            <el-tooltip
+              class="item"
+              effect="dark"
+              content="新建对话"
+              placement="top"
+            >
+              <i class="el-icon-circle-plus-outline"></i>
+            </el-tooltip>
+          </div>
+        </div>
+        <div class="chatBoxInput">
+          <el-input
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 4 }"
+            placeholder="请输入您的问题"
+            v-model="message"
+            @keydown.native="handleKeydown"
+          >
+          </el-input>
+          <!-- <button @click="sendMessage">
+          <i class="el-icon-search"></i>
+        </button> -->
+        </div>
+        <div class="tipBottom">
+          <div class="bottomLeft">
+            <p>
+              <svg class="icon svg-icon" aria-hidden="true">
+                <use xlink:href="#icon-feiji"></use>
+              </svg>
+              <span class="tipText">Ctrl + Enter</span>
+            </p>
+            <p>
+              <svg class="icon svg-icon" aria-hidden="true">
+                <use xlink:href="#icon-huiche"></use>
+              </svg>
+              <span class="tipText">Enter</span>
+            </p>
+          </div>
+          <div class="bottomRight">
+            <el-button
+              icon="el-icon-s-promotion"
+              type="primary "
+              @click="sendMessage"
+              >发送</el-button
+            >
+          </div>
+        </div>
+      </div>
+      <el-dialog title="编辑消息" :visible.sync="dialogVisible" width="30%">
+        <el-input type="textarea" v-model="editMessageText" rows="5"></el-input>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="confirmEdit">确认</el-button>
+        </span>
+      </el-dialog>
     </div>
-    <div></div>
-    <el-dialog title="编辑消息" :visible.sync="dialogVisible" width="30%">
-      <el-input type="textarea" v-model="editMessageText" rows="5"></el-input>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="confirmEdit">确认</el-button>
-      </span>
-    </el-dialog>
   </div>
 </template>
 
@@ -426,62 +505,6 @@ export default {
   font-size: 16px;
 }
 
-.chat-input {
-  display: flex;
-  align-items: center;
-  width: 100%;
-  max-width: 1100px;
-  padding: 10px;
-  background-color: #f5f5f5;
-  margin: 10px;
-  height: 56px;
-  align-items: center;
-  background: #fff;
-  border-radius: 20px;
-  box-shadow: 0 12px 24px -16px rgba(54, 54, 73, 0.04),
-    0 12px 40px 0 rgba(51, 51, 71, 0.08), 0 0 1px 0 rgba(44, 44, 54, 0.02);
-  box-sizing: border-box;
-  position: relative;
-  border: 1px solid #fff;
-}
-
-.chat-input input {
-  flex: 1;
-  padding: 10px 15px;
-  border-radius: 30px;
-  border: none;
-  background-color: #fff;
-  color: #333;
-  outline: none;
-  margin-right: 10px;
-  font-size: 14px;
-  border: 1px solid #fff;
-}
-
-.chat-input:hover {
-  border-color: #35f;
-}
-
-.chat-input button {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  border: none;
-  background-color: #d8d8d8;
-  color: #666;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 20px;
-  transition: background-color 0.3s;
-}
-
-.chat-input:hover button {
-  background-color: #35f;
-  color: #fff;
-}
-
 .code-block {
   position: relative;
   border: 1px solid #333;
@@ -572,7 +595,7 @@ export default {
   left: 0;
   right: 0;
   top: 0;
-  bottom: 0;
+  bottom: 300px;
   font-size: 30px;
   color: #2d2d2d;
   display: flex;
@@ -591,6 +614,73 @@ export default {
     width: 46px;
     height: 46px;
     margin-right: 10px;
+  }
+}
+.chatBottom {
+  border-top: 1px solid #dcdfe6;
+  padding: 5px;
+  padding-left: 10px;
+  .chatBottomHeader {
+    height: 40px;
+
+    display: flex;
+    align-items: center;
+    font-size: 22px;
+    color: #57606a;
+    .workItem {
+      margin-right: 15px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+  .tipBottom {
+    color: #808080;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .bottomLeft {
+      p {
+        margin-top: 5px;
+      }
+      .icon {
+        font-size: 16px;
+      }
+      .tipText {
+        font-size: 12px;
+        margin-left: 5px;
+      }
+    }
+    .bottomRight {
+      width: 80px;
+      height: 40px;
+    }
+  }
+}
+::v-deep .chatBoxInput {
+  textarea {
+    border: none;
+    resize: none;
+  }
+}
+.chatMain {
+  display: flex;
+  .sidebarLeft {
+    width: 260px;
+    border-right: 1px solid hsl(var(--border));
+    pointer-events: auto;
+    background: #fff;
+    .sliderContent {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 100%;
+      padding: 4px;
+    }
+    border-right: 1px solid #dcdfe6;
+  }
+  .chat-container {
+    flex: 1;
   }
 }
 </style>
